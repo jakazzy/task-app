444dadc5423381047142d2613353ca8ad36ea062
jakazzy <jakazzy@gmail.com>
2020-04-07 01:57:54 +0000

    add update todo item

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,28 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
 
-//   // TODO: Remove a TODO item by id
-//   return undefined
-// }
+  const params={
+    TableName: todosTable,
+    Key: {  todoId }
+  }
+
+  await docClient.delete(params).promise()
+  // TODO: Remove a TODO item by id
+  
+  return {
+      statusCode: 200,
+      headers: {
+          'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        result: {Item: "Item deleted successfully"}
+      })
+  }
+}
--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,46 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import * as AWS from 'aws-sdk'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
-// import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
-//   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+ const result = updateTodo(updatedTodo, todoId, todosTable)
 
-//   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-//   return undefined
-// }
+
+
+return {
+    statusCode: 200,
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      result
+    })
+}
+}
+
+async function updateTodo( updatedTodo,todoId, todosTable){
+    console.log(`Updated Todo value: ${updatedTodo.name} , ${updatedTodo.dueDate} ${updatedTodo.done}`)
+    await docClient
+    .update({
+            TableName: todosTable,
+    Key: {  todoId },
+    UpdateExpression: "set #n = :a, dueDate = :b, done = :c",
+    ExpressionAttributeValues:{
+      ":a": updatedTodo.name,
+      ":b": updatedTodo.dueDate,
+      ":c": updatedTodo.done
+    },
+    ExpressionAttributeNames:{
+      "#n": "name"
+    },
+    ReturnValues:"UPDATED_NEW"
+    }).promise()
+}
